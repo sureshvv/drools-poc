34800e1794dc42a0a193fc23cfa7f56e4786e28c
suresh <you@example.com>
2019-10-11 15:40:57 +0530

    poc1

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -3,7 +3,21 @@ package com.sample;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.text.ParseException;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+ 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,30 +42,80 @@ import org.kie.api.event.rule.*;
 public class DroolsTest {
 
     public static final void main(String[] args) {
+        InputStreamReader in= new InputStreamReader(System.in);
+        BufferedReader input = new BufferedReader(in);
+ 
+        String str;
+
+        while (true) {
+            try {
+                str = input.readLine();
+            } catch (IOException e) {
+                break;
+            }
+            if (str == null) {
+                break;
+            }
+            JSONObject j1 = getLine(str);
+            String s1 = (String) j1.get("RULEAPI");
+            if (s1 == null) {
+                String t1 = (String) j1.get("temp");
+                String ts1 = (String) j1.get("timestamp");
+                if (t1 != null && ts1 == null) {
+                    TempReading r1 = new TempReading(Integer.parseInt(t1), ts1);
+                    // session.fireAllRules();
+		}
+            } else {
+                String name1 = (String) j1.get("name");
+                String type1 = (String) j1.get("type");
+                String cond1 = (String) j1.get("cond");
+                if (typ1 == "num") {
+                    String flds[] = cond1.split()
+                } else if (typ1 == "str") {
+                    String flds[] = cond1.split()
+                } else if (typ1 == "logical") {
+                    String flds[] = cond1.split()
+                } else if (typ1 == "interval") {
+                    String flds[] = cond1.split()
+                }
+                System.out.println("Type: " + typ1);
+            }
+        }
+    }
+
+    public void build_rule() {
         try {
 	    KieContainer container = new DroolsTest().build(KieServices.Factory.get());
 	    KieSession session = container.newKieSession();
-            // go !
             TempThreshold temp1 = new TempThreshold(35);
             session.insert(temp1);
             CountThreshold cnt1 = new CountThreshold(2);
             session.insert(cnt1);
-            TempReading r1 = new TempReading(35, "20191001:000000000");
-            session.insert(r1);
-            TempReading r2 = new TempReading(35, "20191001:000100000");
-            session.insert(r2);
-            session.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
 
-    public KieContainer build(KieServices kieServices) {
+    public void process_msg() {
+        // session.insert(r1);
+    }
+
+    public static JSONObject getLine(String str) {
+        JSONObject j1 = null;
+        try {
+            j1 = (JSONObject) JSONValue.parse(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return j1;
+    }
+
+    public KieContainer build(KieServices kieServices, String timeLimit) {
 	KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
 	ReleaseId rid = kieServices.newReleaseId("com.sample.drools", "model-test", "1.0-SNAPSHOT");
 	kieFileSystem.generateAndWritePomXML(rid);
 
-	addRule(kieFileSystem, "2m");
+	addRule(kieFileSystem, timeLimit);
 
 	KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
 	kieBuilder.buildAll();
@@ -61,6 +125,37 @@ public class DroolsTest {
 		
 	return kieServices.newKieContainer(rid);
     }
+
+    private void getRule(String name, String type, String cond) {
+        if (cond == "simple") {
+            return getSimpleRule(name, cond)
+        } else if (cond == "logical") {
+            return getLogicalRule(name, cond)
+        } else if (cond == "interval") {
+            return getIntervalRule(name, cond)
+        } else {
+            assert 0
+        }
+
+    private void getSimpleRule(String name, String cond) {
+        String flds[] = cond.split(" ", 1)
+        String ruleStr = "rule " + name + " when\n" +
+            "Map( this[\"" + flds[0] + "\"] " + flds[1] + " )\n" + 
+            "then\n" +
+            "insertLogical( new Fact(\"" + name + "\") );
+            "end";
+        return ruleStr
+    }
+    private void getComplexRule(String name, String cond) {
+        String flds[] = cond.split(" ")
+        String ruleStr = "rule " + name + " when\n" +
+            "exists( Fact(\"" + flds[0] + "\") " + flds[1] + Fact(\"" + flds[2] + "\") )\n" + 
+            "then\n" +
+            "insertLogical( new Fact(\"" + name + "\") );
+            "end";
+        return ruleStr
+    }
+
 
     private void addRule(KieFileSystem kieFileSystem, String timeLimit) {
         String ruleStr = "import " + TempThreshold.class.getCanonicalName() + ";\n" +
@@ -132,7 +227,7 @@ public class DroolsTest {
         public void setTimestamp(String eventTimestamp) {
             try {
                 this.timestamp = DATE_FORMAT.parse(eventTimestamp.trim());
-	    } catch (ParseException pe) {
+	    } catch (Exception pe) {
              LOGGER.error("Error parsing timestamp: " + eventTimestamp);
          }
         }
